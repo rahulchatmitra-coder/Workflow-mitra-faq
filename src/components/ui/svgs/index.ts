e2b200110187c4df25e0f682e752daf3a39fe935
Migration Engineer <migration-agent@workflowmitra.com>
2026-08-08 17:24:11 +0530

    feat: add central export index for all SVGL icons

---
+++ src/components/ui/svgs/index.ts
@@ -0,0 +1,18 @@
+export { GithubDark } from "./githubDark";
+export { GithubLight } from "./githubLight";
+export { GithubWordmarkDark } from "./githubWordmarkDark";
+export { GithubWordmarkLight } from "./githubWordmarkLight";
+
+export { SanityDark } from "./sanityDark";
+export { SanityLight } from "./sanityLight";
+export { SanityWordmarkDark } from "./sanityWordmarkDark";
+export { SanityWordmarkLight } from "./sanityWordmarkLight";
+
+export { Supabase } from "./supabase";
+export { SupabaseWordmarkDark } from "./supabaseWordmarkDark";
+export { SupabaseWordmarkLight } from "./supabaseWordmarkLight";
+
+export { Vercel } from "./vercel";
+export { VercelDark } from "./vercelDark";
+export { VercelWordmark } from "./vercelWordmark";
+export { VercelWordmarkDark } from "./vercelWordmarkDark";
