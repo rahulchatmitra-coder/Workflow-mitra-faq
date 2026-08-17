4ac11d8c87ed25d307da10dc008ce1504913defb
Rahul Parmar <rahul.chatmitra@gmail.com>
2026-08-17 11:00:41 +0530

    fix: update server and preview ports in vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -73,10 +73,10 @@ export default defineConfig({
   },
   server: {
     host: true,
-    port: 3000,
+    port: 5173,
     allowedHosts: ["192.168.1.46", "localhost"],
   },
   preview: {
-    port: 3001,
+    port: 4173,
   },
 });
